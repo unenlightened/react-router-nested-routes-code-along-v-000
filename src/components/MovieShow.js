6430eba27f1f7f8dd2e3615010508a490e774f81
Chely Ho <adept_inept@yahoo.com>
2018-09-28 22:33:02 +0000

    automatically backed up by learn

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const MovieShow = props => {
+const MovieShow = ({ match, movies }) => {
 
   return(
     <div>
-      <h3>Movies Show Component!</h3>
+      <h3>{ movies[match.params.movieId].title }</h3>
     </div>
   )
 }
