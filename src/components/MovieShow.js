231fc0e23982bdd2b1045b8dfbde05dc1e54f013
Chely Ho <adept_inept@yahoo.com>
2018-09-28 22:24:49 +0000

    automatically backed up by learn

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const MovieShow = ({ movies }) => {
-  const renderMovies = Object.keys(movies).map(movieID =>
-    <Link key={movieID} to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
-  )
-
+const MovieShow = props => {
+  
   return(
     <div>
-      {renderMovies}
+      <h3>Movies Show Component</h3>
     </div>
   )
 }
