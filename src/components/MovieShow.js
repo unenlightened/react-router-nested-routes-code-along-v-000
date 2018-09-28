a7fee96ce1f0b799cc660f472d2370472588685b
Chely Ho <adept_inept@yahoo.com>
2018-09-28 22:25:05 +0000

    automatically backed up by learn

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const MovieShow = props => {
-  
+
   return(
     <div>
-      <h3>Movies Show Component</h3>
+      <h3>Movies Show Component!</h3>
     </div>
   )
 }
